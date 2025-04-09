fabc220c124e21f80be7c07aa11bb8c04a4049a9
Refreshe141 <157901232+Refreshe141@users.noreply.github.com>
2025-04-09 16:28:06 +0900

    매뉴 정보
    메뉴 정보 확인중 
    
    public static void menuIntroduction() {
    		System.out.println("1. 고객정보 확인하기 \t\t4. 바구니에 항목 추가하기");
    		System.out.println("2. 장바구니상품 목록 보기 \t5. 장바구니의항목 수량 줄이기");
    		System.out.println("3.장바구니 비우기 \t\t6. 장바구니의 항목 삭제하기");
    		System.out.println("7.영수증 표시하기 \t\t8. 종료");
    		System.out.println("**************************************");	 부분이 정렬이 되도록 수정

--- bookmarket01.java
+++ bookmarket01.java
@@ -79,10 +79,10 @@ public class bookmarket01 {
 		}
 	}
 	public static void menuIntroduction() {
-		System.out.println("1. 고객정보 확인하기 \t4. 바구니에 항목 추가하기");
+		System.out.println("1. 고객정보 확인하기 \t\t4. 바구니에 항목 추가하기");
 		System.out.println("2. 장바구니상품 목록 보기 \t5. 장바구니의항목 수량 줄이기");
-		System.out.println("3.장바구니 비우기 \t6. 장바구니의 항목 삭제하기");
-		System.out.println("7.영수증 표시하기 \t8. 종료");
+		System.out.println("3.장바구니 비우기 \t\t6. 장바구니의 항목 삭제하기");
+		System.out.println("7.영수증 표시하기 \t\t8. 종료");
 		System.out.println("**************************************");	
 	}
 	public static void menuGuestInfo(String name,String mobile) {
